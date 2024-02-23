ca2fe70691eac407b695a8afa3d34ca48cc9a1d4
yaotutu <957810668@qq.com>
2024-02-23 20:47:29 +0800

    update

--- app/lib/systeminfo/index.ts
+++ app/lib/systeminfo/index.ts
@@ -1,19 +1,27 @@
 "use server";
 import { cpu, currentLoad, processes } from "systeminformation";
 export async function getSystemStaicInfo() {
-  return await cpu();
+    return await cpu();
 }
 
 export async function getCpuCurrentLoad() {
-  const cpuLoad = await currentLoad();
-  return cpuLoad.currentLoad;
+    const cpuLoad = await currentLoad();
+    return cpuLoad.currentLoad;
+
 }
 
-export const getProcessesSortedByCpuUsage = async (optios?: {limit?:number}) => {
-  const {limit = 5} = optios || {};
-  const allProcess = await processes();
-  allProcess.list.sort((a: any, b: any) => {
-    return b.cpu - a.cpu;
-  });
-  return allProcess.list.slice(0, limit);
+export const getProcessesSortedByCpuUsage = async (options?: {
+    limit?: number;
+}) => {
+    const { limit = 5 } = options || {};
+    const allProcess = await processes();
+    allProcess.list.sort((a: any, b: any) => {
+        return b.cpu - a.cpu;
+            });
+  
+    return allProcess.list.slice(0, limit);
 };
+
+if (1 >= -0) {
+    console.log("1 is greater than or equal to -0");
+}
